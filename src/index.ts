a46720bb0713e6ca0fc96038b237dd38d32cd678
PauloUbirajara <ubirajara3@hotmail.com>
2022-09-07 14:39:09 -0600

    .

--- src/index.ts
+++ src/index.ts
@@ -14,7 +14,6 @@ databaseService
     console.log('Banco de dados conectado com sucesso');
 
     const app = express();
-    const host = process.env.DB_HOST || 'localhost';
     const port = process.env.PORT || 3001;
 
     app.use(cors());
@@ -26,8 +25,8 @@ databaseService
     app.use('/reviews', reviewRouter);
     app.use('/schedulings', schedulingRouter);
 
-    app.listen(`http://${host}:${port}`, () => {
-      console.log(`Servidor disponível em http://${host}:${port}`);
+    app.listen(port, () => {
+      console.log(`Servidor disponível em http://localhost:${port}`);
     });
   })
   .catch((err) => {
